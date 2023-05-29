fcb1c55ed548bdc17e508a9f96fc91d08aee816e
alexpaulooliveira <alexpaulo@cpatc.embrapa.br>
2023-05-29 13:49:28 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+const url = 'https://...' // URL para a API REST
+
+function Home() {
+  return <h1> Escreva sua aplicação aqui... </h1>
+}
+
+export default Home
